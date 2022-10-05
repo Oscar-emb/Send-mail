be64493a5a16d3899523d969dad4c714a77232ae
Blessed Oscar <oscarblessed04@gmail.com>
2022-10-05 04:25:48 +0100

    final changes

--- views.py
+++ views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
-from django.views import View
+# from django.views import View
 import random
 # Create your views here.
 
